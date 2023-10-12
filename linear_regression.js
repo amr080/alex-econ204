cffe6a0b3d895010fbfed20c7257759f9c2b2d5d
amr080 <117864716+amr080@users.noreply.github.com>
2023-10-11 22:52:55 -0400

    Update linear_regression.js

--- linear_regression.js
+++ linear_regression.js
@@ -1,52 +1,55 @@
+// linear_regression.js
+
+// Function to fetch OpenAI response
 async function fetchOpenAIResponse(prompt, apiKey) {
-    const response = await fetch('/api/openai', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-      },
-      body: JSON.stringify({ prompt })
-    });
-    const data = await response.json();
-    return data.text;
-  }
-  
-  async function performLinearRegression(y, x, apiKey) {
-    if (y.length !== x.length) {
-      alert("Error: # of elements in dependent + independent variables must be the same.");
-      return;
-    }
-  
-    const xWithConstant = x.map(val => [1, val]); // Add a constant term for the intercept
-    const regressionModel = new ML.SimpleLinearRegression();
-    regressionModel.fit(xWithConstant, y);
-  
-    const regressionResult = {
-      slope: regressionModel.slope,
-      intercept: regressionModel.intercept,
-      rSquared: regressionModel.score(xWithConstant, y)
-    };
-  
-    const summaryText = `Slope: ${regressionResult.slope}\nIntercept: ${regressionResult.intercept}\nR-squared: ${regressionResult.rSquared}`;
-  
-    const prompt = `Please interpret the following regression results:\n${summaryText}`;
-    const interpretation = await fetchOpenAIResponse(prompt, apiKey);
-  
-    const resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = `<h3>Regression Results:</h3><pre>${summaryText}</pre><h3>OpenAI API Interpretation:</h3><p>${interpretation}</p>`;
-  }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    const yInput = document.getElementById('y-input');
-    const xInput = document.getElementById('x-input');
-    const apiKeyInput = document.getElementById('api-key-input');
-    const submitButton = document.getElementById('submit-button');
-  
-    submitButton.addEventListener('click', () => {
-      const y = yInput.value.split(',').map(Number);
-      const x = xInput.value.split(',').map(Number);
-      const apiKey = apiKeyInput.value;
-      performLinearRegression(y, x, apiKey);
-    });
+  const response = await fetch('/api/openai', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({ prompt })
   });
-  
+  const data = await response.json();
+  return data.text;
+}
+
+// Function to perform linear regression
+async function performLinearRegression(y, x, apiKey) {
+  const response = await fetch('/api/regression', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ y, x })
+  });
+  const data = await response.json();
+  const summaryText = data.summary;
+
+  const prompt = `Please interpret the following regression results:\n${summaryText}`;
+  const interpretation = await fetchOpenAIResponse(prompt, apiKey);
+  console.log(`OpenAI API Interpretation: ${interpretation}`);
+
+  let userQuestion = prompt("Do you have any questions about the regression results? (type 'exit' to quit): ");
+  while (userQuestion.toLowerCase() !== 'exit') {
+    const prompt = `Based on the following regression results, ${userQuestion}:\n${summaryText}`;
+    const answer = await fetchOpenAIResponse(prompt, apiKey);
+    console.log(`OpenAI API Answer: ${answer}`);
+    userQuestion = prompt("Do you have any questions about the regression results? (type 'exit' to quit): ");
+  }
+}
+
+// Add an event listener to execute linear regression when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+  const yInput = document.getElementById('y-input');
+  const xInput = document.getElementById('x-input');
+  const apiKeyInput = document.getElementById('api-key-input');
+  const submitButton = document.getElementById('submit-button');
+
+  submitButton.addEventListener('click', () => {
+    const y = yInput.value.split(',').map(Number);
+    const x = xInput.value.split(',').map(Number);
+    const apiKey = apiKeyInput.value;
+    performLinearRegression(y, x, apiKey);
+  });
+});
